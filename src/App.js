adf94f973ad0e7a1fb9db189d074dcdc5deed314
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-03-28 12:20:25 +0200

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from './components/Header'
+import Main from './components/Main'
+
 
 class App extends Component {
   render() {

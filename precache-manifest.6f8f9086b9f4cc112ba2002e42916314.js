f6efc07d6be31c5c1932b2604499cc3dd5dcc0d2
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-03-28 13:17:34 +0200

    Updates

---
+++ precache-manifest.6f8f9086b9f4cc112ba2002e42916314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52c2f9d209346cdd1c7f",
+    "url": "/static/css/main.5ceba9fd.chunk.css"
+  },
+  {
+    "revision": "52c2f9d209346cdd1c7f",
+    "url": "/static/js/main.4a6c61b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "833d1d34d0fa31898e12",
+    "url": "/static/js/2.1105526d.chunk.js"
+  },
+  {
+    "revision": "169ca5a93f85d3290020b064da6b1da2",
+    "url": "/index.html"
+  }
+];
